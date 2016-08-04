4094561c862b36ee9c1aa5356e9d816a2be5dbbf
Sergey <Sergey.Golovaschuk@migonsoftware.com>
2016-08-04 22:36:08 +0300

    branch77_test_commit

--- serg1/Main.java
+++ serg1/Main.java
@@ -10,32 +10,31 @@ public class Main {
         for (IdProvider validated : IdProvider.values()) {
             if (validated.mId.length() < 10) try {
                 throw new Exception((String.format("%s id length < 10", validated.ordinal())));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            } catch (Exception e) {e.printStackTrace();}
             if (validated.mId.matches(".*[a-zA-Z]+.*")) try {
                 throw new Exception(String.format("%s id contains string", validated.ordinal()));
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            } catch (Exception e) {e.printStackTrace();}
         }
     }
-        public enum IdProvider{
-            one("9999999999"),
-            two("7777777777"),
-            three("888888"),
-            four("1111111111"),
-            five("2222222222"),
-            six("3333333333"),
-            seven("4444444444"),
-            eight("5555555"),
-            nine("vfnfggn"),
-            ten("00000000000");
-            private IdProvider(String id){
-                mId = id;
-            }
-            String mId;
+
+    public enum IdProvider {
+        one("9999999999"),
+        two("7777777777"),
+        three("888888"),
+        four("1111111111"),
+        five("2222222222"),
+        six("3333333333"),
+        seven("4444444444"),
+        eight("5555555"),
+        nine("vfnfggn"),
+        ten("00000000000");
+
+        private IdProvider(String id) {
+            mId = id;
         }
+
+        String mId;
+    }
 
 
 }
