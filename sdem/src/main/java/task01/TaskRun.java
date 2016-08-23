1ebcfed452d472f0349abad69282ce387c316e0f
sdemenev <sevladem@gmail.com>
2016-08-23 09:56:14 +0300

    test 23/08/2016

---
+++ sdem/src/main/java/task01/TaskRun.java
@@ -0,0 +1,13 @@
+package task01;
+
+/**
+ * Created by SDem on 22.08.2016.
+ * my task01 main unit
+ */
+public class TaskRun {
+
+    public static void main(String[] args) {
+        System.out.println("Hello my dear friend");
+    }
+
+}
